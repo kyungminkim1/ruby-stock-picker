bb2db45c61d041bed55b6b5e13c76b4df8b5e764
kyungminkim1 <kyungminkimnz@gmail.com>
2023-01-09 18:14:59 +1300

    Complete stock_picker

--- stock-picker.rb
+++ stock-picker.rb
@@ -12,25 +12,25 @@
     # return index of selling price and buying price as array
 
 def stock_picker(stock_prices)
-    lowest_price = nil
-    lowest_price_index = nil
-    for i in 0...stock_prices.length - 1 do
-        if lowest_price == nil || lowest_price > stock_prices[i]
-            lowest_price = stock_prices[i]
-            lowest_price_index = i
+    highest_profit = nil
+    buying_price_index = nil
+    selling_price_index = nil
+    # exclude last day to reserve for selling day
+    for buying_i in 0...stock_prices.length - 1 do
+        # exclude days before buying day
+        for selling_i in buying_i + 1...stock_prices.length do
+            profit = stock_prices[selling_i] - stock_prices[buying_i]
+            if highest_profit == nil || highest_profit < profit
+                highest_profit = profit
+                buying_price_index = buying_i
+                selling_price_index = selling_i
+            end
         end
     end
-    highest_price = nil
-    highest_price_index = nil
-    for i in lowest_price_index...stock_prices.length do
-        if highest_price == nil || highest_price < stock_prices[i]
-            highest_price = stock_prices[i]
-            highest_price_index = i
-        end
-    end
-    puts "profit of $#{highest_price} - $#{lowest_price}
-     = $#{highest_price - lowest_price}"
-    return [lowest_price_index, highest_price_index]
+
+    puts "profit of $#{stock_prices[selling_price_index]} - $#{stock_prices[buying_price_index]} = \
+$#{stock_prices[selling_price_index] - stock_prices[buying_price_index]}"
+    return [buying_price_index, selling_price_index]
 end
 
 buy_and_sell_index = stock_picker([17,3,6,9,15,8,6,1,10])
