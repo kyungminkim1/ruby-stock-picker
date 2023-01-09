dbd4b84ae6623f78838eab241717b4efe297e244
kyungminkim1 <kyungminkimnz@gmail.com>
2023-01-09 13:05:32 +1300

    Create stock_picker pseudocode

---
+++ stock-picker.rb
@@ -0,0 +1,8 @@
+# function stock_picker takes one argument: stock_prices (array)
+    # loop through stock_prices
+    # excluding the last element:
+        # save index of the lowest number as buying price
+    # loop through stock_prices
+    # excluding elements before buying price index
+        # save index of highest number as selling price
+    # return lowest number and highest number as array
