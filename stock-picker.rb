f61554d12a2529deec6aa9d8babac710f70b1805
kyungminkim1 <kyungminkimnz@gmail.com>
2023-01-09 14:54:36 +1300

    Create stock_picker function Current version returns incorrect result. It finds the lowest number and uses its index as starting point to find highest number, so it returns [1,10] when it should return [1,4] instead.
    To fix: Add nested loop that loops through array starting after
    point where the parent loop begins e.g. if parent loop starts at
    index 2, child loop should start at index 3.
    
    Use it to calculate and save profit value to compare.

--- stock-picker.rb
+++ stock-picker.rb
@@ -6,3 +6,28 @@
     # excluding elements before buying price index
         # save index of highest number as selling price
     # return lowest number and highest number as array
+
+def stock_picker(stock_prices)
+    lowest_price = nil
+    lowest_price_index = nil
+    for i in 0...stock_prices.length - 1 do
+        if lowest_price == nil || lowest_price > stock_prices[i]
+            lowest_price = stock_prices[i]
+            lowest_price_index = i
+        end
+    end
+    highest_price = nil
+    highest_price_index = nil
+    for i in lowest_price_index...stock_prices.length do
+        if highest_price == nil || highest_price < stock_prices[i]
+            highest_price = stock_prices[i]
+            highest_price_index = i
+        end
+    end
+    puts "profit of $#{highest_price} - $#{lowest_price}
+     = $#{highest_price - lowest_price}"
+    return [lowest_price_index, highest_price_index]
+end
+
+buy_and_sell_index = stock_picker([17,3,6,9,15,8,6,1,10])
+p buy_and_sell_index
